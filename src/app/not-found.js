01bb7e878c30b9767ab738c2bb6bc20f80630950
nkarimovic <n.karimovic@gmail.com>
2024-10-25 09:31:19 +0200

    added slider webp images

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,3 @@
-
-import CtaThree from "~/components/Section/Common/Cta/CtaThree";
-import FooterThree from "~/components/Section/Common/Footer/FooterThree";
-import { HeaderFour } from "~/components/Section/Common/Header";
 import PageHeader from "~/components/Section/Common/PageHeader";
 import ErrorSection from "~/components/Section/ErrorSection";
 import Header from "~/components/Section/Common/Header";
